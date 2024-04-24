5e8f71a4a1b67533050254a96c32c5bdbbf84367
aboud <abderrahmen.trabelsi@esprit.tn>
2024-04-24 10:58:41 +0100

    🔀 chore(.env): remove outdated .env file and add new .env file for API with updated DB credentials 🔧 fix(main.go): update GORM MySQL driver import path and correct DSN format for MySQL connection 🔧 fix(go.mod): add gorm.io/driver/mysql v1.5.6 as an indirect dependency 🔧 fix(go.sum): add gorm.io/driver/mysql v1.5.6 and update gorm.io/gorm to v1.25.9 🔧 fix(database.go): refactor database initialization to ensure database existence and improve error handling

--- back/cmd/api/main.go
+++ back/cmd/api/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"back/internal/model"
 	"back/internal/server"
+	"fmt"
+	"gorm.io/driver/mysql" // This is the correct import path for GORM's MySQL driver
+	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 func main() {
-
 	server := server.NewServer()
 
-	err := server.ListenAndServe()
+	// Use the correct DSN format for MySQL connection string for GORM
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to initialize GORM: %v", err)
+	}
+
+	// Auto migrate your models
+	err = db.AutoMigrate(&models.User{}, &models.Department{}, &models.LoginHistory{}, &models.WorkHours{}, &models.TokenDetails{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database models: %v", err)
+	}
+
+	err = server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
--- back/go.mod
+++ back/go.mod
@@ -37,5 +37,6 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.6 // indirect
 	gorm.io/gorm v1.25.9 // indirect
 )
