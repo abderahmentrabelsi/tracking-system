8b8672f5810c65513d218a9662927be4750ab3c1
aboud <abderrahmen.trabelsi@esprit.tn>
2024-05-16 17:38:38 +0100

    🐛 fix(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics ✨ feat(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port ✨ feat(client): add Signup page component ✨ feat(client): add DirectionalIcon component ✨ feat(client): add VerticalMenu item for Signup page ✨ feat(client): add navigation item for Signup page ✨ feat(client): add Signup view component

--- client/src/data/navigation/verticalMenuData.tsx
+++ client/src/data/navigation/verticalMenuData.tsx
@@ -11,6 +11,11 @@ const verticalMenuData = (): VerticalMenuDataType[] => [
     label: 'About',
     href: '/about',
     icon: 'tabler-info-circle'
+  },
+  {
+    label: 'Sign Up',
+    href: '/signup',
+    icon: 'tabler-user-plus'
   }
 ]
 
