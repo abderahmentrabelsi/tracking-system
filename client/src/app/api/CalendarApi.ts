7407d2d510094eb1509a70f92dabc5c47ac9b68d
aboud <abderrahmen.trabelsi@esprit.tn>
2024-08-03 16:02:58 +0100

    🔧 chore(CalendarApi.ts): update baseURL to use environment variable
    - Changed the baseURL in axios client to use process.env.NEXT_PUBLIC_GO_APP_SERVER_URL.
    - Ensured all calendar-related API calls are consistent with the new base URL setup.

--- client/src/app/api/CalendarApi.ts
+++ client/src/app/api/CalendarApi.ts
@@ -2,32 +2,30 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8383',
+  baseURL: process.env.NEXT_PUBLIC_GO_APP_SERVER_URL,
   withCredentials: true,
 });
 
-
 export const createCalendar = async (calendar: any) => {
-  return  api.post('/calendars', calendar);
+  return api.post('/calendars', calendar);
 };
 
 export const getCalendarByDepartmentId = (departmentId: number) => {
   return api.get(`/calendars/department/${departmentId}`);
 };
 
-
 export const getCalendarByID = (calendarId: number) => {
-  return api.get(`/calendars/:calendar_id`);
+  return api.get(`/calendars/${calendarId}`);
 };
 
 export const createEvent = (calendarId: number, eventData: any) => {
   return api.post(`/calendars/${calendarId}/events`, eventData);
 };
 
-export const updateEvent = (calendarId: number, eventId :number ,eventData: any) => {
+export const updateEvent = (calendarId: number, eventId: number, eventData: any) => {
   return api.put(`/calendars/${calendarId}/events/${eventId}`, eventData);
 };
 
-export const deleteEvent = (calendarId: number , eventId :number) => {
+export const deleteEvent = (calendarId: number, eventId: number) => {
   return api.delete(`/calendars/${calendarId}/events/${eventId}`);
 };
